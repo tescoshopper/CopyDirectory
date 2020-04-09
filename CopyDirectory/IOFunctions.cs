1d0fef6febc0ce0a29f5ab703cfd8691736fafe2
tescoshopper <52611145+tescoshopper@users.noreply.github.com>
2020-04-09 17:11:42 +0100

    Add files via upload

---
+++ CopyDirectory/IOFunctions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CopyDirectory
+{
+    /// <summary>
+    /// Represents event arguments for copied file.
+    /// </summary>
+    public class FileCopiedEventArgs : EventArgs
+    {
+        public string File { get; set; }
+    }
+
+    /// <summary>
+    /// Copy Directiory IO functions.
+    /// </summary>
+    public static class IOFunctions
+    {
+        public static event EventHandler<FileCopiedEventArgs> FileCopied;
+
+        /// <summary>
+        /// Delegate to handle action when file copied.
+        /// </summary>
+        /// <param name="e">Event arguments.</param>
+        public static void OnFileCopied(FileCopiedEventArgs e)
+        {
+            EventHandler<FileCopiedEventArgs> handler = FileCopied;
+            handler?.Invoke(null, e);
+        }
+
+        /// <summary>
+        /// Function to recursively copy a source folder to a destination folder.
+        /// </summary>
+        /// <param name="sourceFolder">Full path of source folder.</param>
+        /// <param name="destinationFolder">Full path of destination folder.</param>
+        public static async Task CopyFilesAsync(string sourceFolder, string destinationFolder)
+        {
+            Directory.CreateDirectory(destinationFolder);
+            foreach (string filename in Directory.EnumerateFiles(sourceFolder))
+            {
+                using (FileStream SourceStream = File.Open(filename, FileMode.Open))
+                {
+                    using (FileStream DestinationStream = File.Create(destinationFolder + filename.Substring(filename.LastIndexOf('\\'))))
+                    {
+                        await SourceStream.CopyToAsync(DestinationStream);
+                        OnFileCopied(new FileCopiedEventArgs() { File = filename });
+                    }
+                }
+            }
+
+            foreach (string folder in Directory.EnumerateDirectories(sourceFolder))
+            {
+               await CopyFilesAsync(folder, destinationFolder + folder.Substring(folder.LastIndexOf('\\')));
+            }
+        }
+    }
+}
---
+++ TestHarness/Program.cs
@@ -0,0 +1,46 @@
+﻿using CopyDirectory;
+using System;
+using System.Threading.Tasks;
+
+namespace TestHarness
+{
+    class Program
+    {
+         static async Task<int> Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: <Source Folder> <Destination Folder>");
+                return 1;
+            }
+
+            // Handle all errors both expected and unexpected at this time.
+            // Note this is a test harness only.
+            try
+            {
+                Console.WriteLine("Copying Files ...");
+                Console.WriteLine();
+
+                IOFunctions.FileCopied += IOFunctions_FileCopied;
+                Task copyFiles = IOFunctions.CopyFilesAsync(args[0], args[1]);
+
+                for (int i = 0; i < 50000; i++)
+                {
+                    Console.WriteLine($"Doing something else in the UI {i}");
+                }
+                await copyFiles;
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Operation aborted with the following error:");
+                Console.WriteLine(ex.Message);
+                return 1;
+            }
+        }
+        private static void IOFunctions_FileCopied(object sender, FileCopiedEventArgs e)
+        {
+            Console.WriteLine(e.File);
+        }
+    }
+}
